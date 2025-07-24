34b05dfc29d048dea561546dfbc1f127329376e8
ASC04-CHANDRA-GIT-HUB <pes22021089@pesu.pes.edu>
2025-07-24 09:41:59 +0530

    friendsTst

--- msSQLserver/friendsTst.sql
+++ msSQLserver/friendsTst.sql
@@ -1,11 +1,11 @@
-CREATE TABLE contacts (
+CREATE TABLE friendsTst (
     ID INT PRIMARY KEY,
     fullName VARCHAR(100),
     emailAddr VARCHAR(100),
     phoneNumber BIGINT
 );
 
-INSERT INTO contacts (ID, fullName, emailAddr, phoneNumber)
+INSERT INTO friendsTst (ID, fullName, emailAddr, phoneNumber)
 VALUES 
 (1, 'John Doe', 'john@example.com', 9876543210),
 (2, 'Alice Smith', 'alice@example.com', 9123456789),
