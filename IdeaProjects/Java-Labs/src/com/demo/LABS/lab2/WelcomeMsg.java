7666ff545a93da3e1485938f03a4bd965bdd51ee
ASC04-CHANDRA-GIT-HUB <pes22021089@pesu.pes.edu>
2025-07-31 15:41:49 +0530

    update - 2025-07-31 15:41:48

--- IdeaProjects/Java-Labs/src/com/demo/LABS/lab2/WelcomeMsg.java
+++
@@ -1,9 +0,0 @@
-package com.demo.LABS.lab2;
-
-public class WelcomeMsg{
-    public static String welcomemsg(String name){return("Hello "+name+" welcome to the world! ");}
-    public static void main(String[] args){
-        String msg=welcomemsg("Boss!");
-        System.out.println(msg);
-    }
-}
--- msSQLserver/gflights.sql
+++ msSQLserver/gflights.sql
@@ -1,3 +1,5 @@
+use myfriends_db;
+
 CREATE TABLE bookings (
     bookingId INT PRIMARY KEY,
     source VARCHAR(100),
@@ -6,3 +8,14 @@ CREATE TABLE bookings (
     numPassengers INT,
     isRoundTrip BIT
 );
+
+
+INSERT INTO bookings (bookingId, source, destination, ticketClass, numPassengers, isRoundTrip)
+VALUES 
+(1, 'New York', 'Los Angeles', 'Economy', 2, 1),
+(2, 'Chicago', 'Miami', 'Business', 1, 0),
+(3, 'San Francisco', 'Seattle', 'First Class', 3, 1);       
+
+select * from bookings;
+
+
